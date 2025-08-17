a2f386ca38ac97ab1aa43dbe3a5b4382b4788f30
David Jideofor <jide.remote@gmail.com>
2025-08-17 21:35:36 +0100

    fix: corrected the address values

--- task_5.sql
+++ task_5.sql
@@ -2,4 +2,4 @@
 -- Insert a single row into the customer table
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo' 'cbaidoo@sandtech.com', '123 Happiness Ave');
+VALUES (1, 'Cole Baidoo' 'cbaidoo@sandtech.com', '123 Happiness Ave.');
